6bd89836a9ef4787d9d153e026cb88f2c5cea34b
liukai <zhizhufan@foxmail.com>
2020-09-07 09:33:01 +0800

    update: CustomizeRepository.java

--- src/main/java/com/unicom/framework/repository/CustomizeRepository.java
+++ src/main/java/com/unicom/framework/repository/CustomizeRepository.java
@@ -2,9 +2,11 @@ package com.unicom.framework.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
 
 /**
  * @author liukai
  */
+@NoRepositoryBean
 public interface CustomizeRepository<T, ID> extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
 }
