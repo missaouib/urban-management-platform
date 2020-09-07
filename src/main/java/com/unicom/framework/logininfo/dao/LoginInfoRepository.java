23077aab1d5dc7117495e4a5313380a11215fad9
liukai <zhizhufan@foxmail.com>
2020-09-07 09:20:37 +0800

    add: CustomizeRepository.java

--- src/main/java/com/unicom/framework/logininfo/dao/LoginInfoRepository.java
+++ src/main/java/com/unicom/framework/logininfo/dao/LoginInfoRepository.java
@@ -1,10 +1,9 @@
 package com.unicom.framework.logininfo.dao;
 
 import com.unicom.framework.logininfo.entity.LoginInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import com.unicom.framework.repository.CustomizeRepository;
 
-public interface LoginInfoRepository extends JpaRepository<LoginInfo, String>, JpaSpecificationExecutor<LoginInfo> {
+public interface LoginInfoRepository extends CustomizeRepository<LoginInfo, String> {
 
 
 }
--- src/main/java/com/unicom/project/user/dao/UserRepository.java
+++ src/main/java/com/unicom/project/user/dao/UserRepository.java
@@ -1,10 +1,11 @@
 package com.unicom.project.user.dao;
 
+import com.unicom.framework.repository.CustomizeRepository;
 import com.unicom.project.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface UserRepository extends JpaRepository<User, String>, JpaSpecificationExecutor<User> {
+public interface UserRepository extends CustomizeRepository<User, String> {
 
     User findByUsername(String username);
 
