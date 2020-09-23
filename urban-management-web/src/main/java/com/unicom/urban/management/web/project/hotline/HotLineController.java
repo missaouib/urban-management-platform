b5bc8805355c48fd403329d8579e8d5dcecbf56a
liukai <zhizhufan@foxmail.com>
2020-09-23 09:46:06 +0800

    add: 热线上报页面

---
+++ urban-management-web/src/main/java/com/unicom/urban/management/web/project/hotline/HotLineController.java
@@ -0,0 +1,22 @@
+package com.unicom.urban.management.web.project.hotline;
+
+import com.unicom.urban.management.common.annotations.ResponseResultBody;
+import com.unicom.urban.management.common.constant.SystemConstant;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 热线上报
+ */
+@RestController
+@ResponseResultBody
+public class HotLineController {
+
+    @GetMapping("/hotline")
+    public ModelAndView hotline() {
+        return new ModelAndView(SystemConstant.PAGE + "/hotline/hotline");
+    }
+
+
+}
