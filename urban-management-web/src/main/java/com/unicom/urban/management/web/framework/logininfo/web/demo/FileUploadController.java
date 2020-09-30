a41408491f97d6aca6182c61ba57f4ab741e4e4c
liukai <zhizhufan@foxmail.com>
2020-09-30 13:10:02 +0800

    add: 文件上传common.html

---
+++ urban-management-web/src/main/java/com/unicom/urban/management/web/framework/logininfo/web/demo/FileUploadController.java
@@ -0,0 +1,22 @@
+package com.unicom.urban.management.web.framework.logininfo.web.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 文件上传
+ *
+ * @author liukai
+ */
+@Controller
+@RequestMapping("/demo/upload")
+public class FileUploadController {
+
+
+
+
+
+
+
+}
