e251e787630e1ba16dac43eb17d7f95ba9805181
wangzhe <965270040@qq.com>
2020-10-09 09:03:30 +0800

    feat:退出按钮

--- urban-management-web/src/main/resources/static/js/main.js
+++ urban-management-web/src/main/resources/static/js/main.js
@@ -3,8 +3,11 @@ $(function() {
         $(this).addClass("active").siblings(".circle").removeClass("active");
         $(".test").eq($(this).index(".circle")).addClass("testActive").siblings(".test").removeClass("testActive");
         $(".work").eq($(this).index(".circle")).addClass("workActive").siblings(".work").removeClass("workActive");
-
-        // $(".circle .other").eq($(this).index(".circle")).addClass("otherActive").siblings(".circle .other").removeClass("otherActive");
         $(this).children(".other").addClass("otherActive").parent(".circle").siblings('.circle').children(".other").removeClass("otherActive");
+
+    })
+    // 退出点击事件
+    $(".quit").click(function () {
+        alert(1)
     })
 })
