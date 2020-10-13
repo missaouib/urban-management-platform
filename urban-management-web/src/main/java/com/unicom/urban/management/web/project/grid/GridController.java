4048f231acf9a509eb8062e6a4ad3a61d8c2cfe5
liukai <zhizhufan@foxmail.com>
2020-10-13 11:00:03 +0800

    add: 网格管理

---
+++ urban-management-web/src/main/java/com/unicom/urban/management/web/project/grid/GridController.java
@@ -0,0 +1,22 @@
+package com.unicom.urban.management.web.project.grid;
+
+import com.unicom.urban.management.common.annotations.ResponseResultBody;
+import com.unicom.urban.management.common.constant.SystemConstant;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 网格管理
+ *
+ * @author liukai
+ */
+@RestController
+@ResponseResultBody
+public class GridController {
+
+    @GetMapping("/grid")
+    public ModelAndView grid() {
+        return new ModelAndView(SystemConstant.PAGE + "/grid/grid");
+    }
+}
