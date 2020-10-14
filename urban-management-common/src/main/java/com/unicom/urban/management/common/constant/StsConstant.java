c52da3cbf8a3b1ebab85c722177498826b11678f
顾志杰 <819162954@qq.com>
2020-10-14 19:15:35 +0800

    feat: sts

---
+++ urban-management-common/src/main/java/com/unicom/urban/management/common/constant/StsConstant.java
@@ -0,0 +1,19 @@
+package com.unicom.urban.management.common.constant;
+
+/**
+ * sts
+ *
+ * @author 顾志杰
+ * @date 2020/10/14-19:12
+ */
+public class StsConstant {
+    /**
+     * 删除
+     */
+    public static final String DELETE = "1";
+
+    /**
+     * 在用
+     */
+    public static final String INUSE = "0";
+}
