4eb4dba47e7b17543183bda555da7a840734bd5f
jiangw <373438073@qq.com>
2020-10-16 09:03:37 +0800

    feat: 地图发布
    地图发布列表

--- urban-management-dao/src/main/java/com/unicom/urban/management/dao/release/PublishRepository.java
+++ urban-management-dao/src/main/java/com/unicom/urban/management/dao/release/PublishRepository.java
@@ -20,4 +20,13 @@ public interface PublishRepository extends CustomizeRepository<Publish, String> 
      */
     List<Publish> findAllByKv_Id(String kvId);
 
+    /**
+     * 根据kvId 和 部件分类id查询 发布
+     *
+     * @param kvId            id
+     * @param componentTypeId id
+     * @return 发布
+     */
+    List<Publish> findAllByKv_IdAndComponentType_id(String kvId, String componentTypeId);
+
 }
