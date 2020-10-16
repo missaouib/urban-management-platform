c6861b6a26aa5660c84e5c956a3f3211f0ccd48f
liukai <zhizhufan@foxmail.com>
2020-10-16 15:21:27 +0800

    feat: 上传文件到本地

--- urban-management-common/src/main/java/com/unicom/urban/management/common/util/FileUploadUtil.java
+++ urban-management-common/src/main/java/com/unicom/urban/management/common/util/FileUploadUtil.java
@@ -1,14 +1,15 @@
 package com.unicom.urban.management.common.util;
 
+import cn.hutool.core.io.FileUtil;
 import com.github.tobato.fastdfs.domain.fdfs.StorePath;
 import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 
 /**
@@ -22,11 +23,25 @@ public class FileUploadUtil extends FileUtils {
     @Autowired
     protected FastFileStorageClient storageClient;
 
-    public String uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+    /**
+     * 上传文件到Fastdfs
+     */
+    public String uploadFileToFastDFS(MultipartFile file) throws IOException {
         String originalFilename = file.getOriginalFilename();
         String extension = FilenameUtils.getExtension(originalFilename);
         StorePath storePath = storageClient.uploadFile(file.getInputStream(), file.getSize(), extension, null);
         return storePath.getFullPath();
+    }
+
+    /**
+     * 上传文件到服务器
+     */
+    public String uploadFileToLocal(MultipartFile file) throws IOException {
+        String tmpDirPath = FileUtil.getTmpDirPath();
+        String path = tmpDirPath + file.getOriginalFilename();
+        File dest = new File(path);
+        file.transferTo(dest);
+        return path;
     }
 
 }
