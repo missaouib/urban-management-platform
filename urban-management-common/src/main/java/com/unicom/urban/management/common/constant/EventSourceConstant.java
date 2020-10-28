ec76a7a436fea4f8fd27b9adc7cad4acd95f2c3f
liukai <zhizhufan@foxmail.com>
2020-10-28 17:03:49 +0800

    feat: 事件来源常量类

---
+++ urban-management-common/src/main/java/com/unicom/urban/management/common/constant/EventSourceConstant.java
@@ -0,0 +1,17 @@
+package com.unicom.urban.management.common.constant;
+
+/**
+ * 事件来源常量
+ *
+ * @author liukai
+ */
+public class EventSourceConstant {
+
+
+    /**
+     * 监督员上报
+     */
+    public static final String SUPERVISE_REPORTING = "fd533d5b-b5d1-4fbc-b4cd-4ced27b7b0c11";
+
+
+}
