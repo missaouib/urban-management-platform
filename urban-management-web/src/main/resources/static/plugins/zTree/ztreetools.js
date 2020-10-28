6157ccdea0aaf741119b67a315664be5b53b10e1
liukai <zhizhufan@foxmail.com>
2020-10-28 14:28:00 +0800

    feat: 事件类型tree

--- urban-management-web/src/main/resources/static/plugins/zTree/ztreetools.js
+++ urban-management-web/src/main/resources/static/plugins/zTree/ztreetools.js
@@ -10,21 +10,24 @@
                 var defaults = {
                     id: "tree",                    // 属性ID
                     expandLevel: 0,                // 展开等级节点
-                    view: {
-                        selectedMulti: false,      // 设置是否允许同时选中多个节点
-                        nameIsHTML: true           // 设置 name 属性是否支持 HTML 脚本
-                    },
-                    check: {
-                        enable: false,             // 置 zTree 的节点上是否显示 checkbox / radio
-                        nocheckInherit: true,      // 设置子节点是否自动继承
-                        chkboxType: { "Y": "ps", "N": "ps" } // 父子节点的关联关系
-                    },
+                    // view: {
+                    //     selectedMulti: false,      // 设置是否允许同时选中多个节点
+                    //     nameIsHTML: true           // 设置 name 属性是否支持 HTML 脚本
+                    // },
+                    // check: {
+                    //     enable: false,             // 置 zTree 的节点上是否显示 checkbox / radio
+                    //     nocheckInherit: true,      // 设置子节点是否自动继承
+                    //     chkboxType: {"Y": "ps", "N": "ps"} // 父子节点的关联关系
+                    // },
                     data: {
                         key: {
-                            title: "title"         // 节点数据保存节点提示信息的属性名称
+                            title: "name",         // 节点数据保存节点提示信息的属性名称
+                            name: "name"
                         },
                         simpleData: {
-                            enable: true           // true / false 分别表示 使用 / 不使用 简单数据模式
+                            enable: true,           // true / false 分别表示 使用 / 不使用 简单数据模式
+                            idKey: 'id',
+                            pIdKey: 'parentId'
                         }
                     },
                 };
@@ -34,30 +37,52 @@
                 var setting = {
                     callback: {
                         onClick: options.onClick,                      // 用于捕获节点被点击的事件回调函数
-                        onCheck: options.onCheck,                      // 用于捕获 checkbox / radio 被勾选 或 取消勾选的事件回调函数
-                        onDblClick: options.onDblClick                 // 用于捕获鼠标双击之后的事件回调函数
+                    //     onCheck: options.onCheck,                      // 用于捕获 checkbox / radio 被勾选 或 取消勾选的事件回调函数
+                    //     onDblClick: options.onDblClick                 // 用于捕获鼠标双击之后的事件回调函数
                     },
-                    check: options.check,
-                    view: options.view,
+                    // check: options.check,
+                    // view: options.view,
                     data: options.data
                 };
-                $.get(options.url, function(data) {
+
+                if (options.url === undefined) {
                     var treeId = $("#treeId").val();
-                    tree = $.fn.zTree.init($("#" + options.id), setting, data);
+                    tree = $.fn.zTree.init($("#" + options.id), setting, options.datas);
                     $._tree = tree;
-                    for (var i = 0; i < options.expandLevel; i++) {
-                        var nodes = tree.getNodesByParam("level", i);
-                        for (var j = 0; j < nodes.length; j++) {
-                            tree.expandNode(nodes[j], true, false, false);
-                        }
-                    }
-                    var node = tree.getNodesByParam("id", treeId, null)[0];
-                    $.tree.selectByIdName(treeId, node);
-                    // 回调tree方法
-                    if(typeof(options.callBack) === "function"){
-                        options.callBack(tree);
-                    }
-                });
+                    // for (var i = 0; i < options.expandLevel; i++) {
+                    //     var nodes = tree.getNodesByParam("level", i);
+                    //     for (var j = 0; j < nodes.length; j++) {
+                    //         tree.expandNode(nodes[j], true, false, false);
+                    //     }
+                    // }
+                    // var node = tree.getNodesByParam("id", treeId, null)[0];
+                    // $.tree.selectByIdName(treeId, node);
+                    // // 回调tree方法
+                    // if (typeof (options.callBack) === "function") {
+                    //     options.callBack(tree);
+                    // }
+
+                } else {
+                    // $.get(options.url, function (data) {
+                    //     var treeId = $("#treeId").val();
+                    //     tree = $.fn.zTree.init($("#" + options.id), setting, data);
+                    //     $._tree = tree;
+                    //     for (var i = 0; i < options.expandLevel; i++) {
+                    //         var nodes = tree.getNodesByParam("level", i);
+                    //         for (var j = 0; j < nodes.length; j++) {
+                    //             tree.expandNode(nodes[j], true, false, false);
+                    //         }
+                    //     }
+                    //     var node = tree.getNodesByParam("id", treeId, null)[0];
+                    //     $.tree.selectByIdName(treeId, node);
+                    //     // 回调tree方法
+                    //     if (typeof (options.callBack) === "function") {
+                    //         options.callBack(tree);
+                    //     }
+                    // });
+                }
+
+
             },
             // 搜索节点
             searchNode: function() {
@@ -79,13 +104,13 @@
                 $.tree.updateNodes($._tree.getNodesByParamFuzzy("name", value));
             },
             // 根据Id和Name选中指定节点
-            selectByIdName: function(treeId, node) {
-                if ($.common.isNotEmpty(treeId) && treeId == node.id) {
-                    $._tree.selectNode(node, true);
-                }
-            },
+            // selectByIdName: function(treeId, node) {
+            //     if ($.common.isNotEmpty(treeId) && treeId == node.id) {
+            //         $._tree.selectNode(node, true);
+            //     }
+            // },
             // 显示所有节点
-            showAllNode: function(nodes) {
+            showAllNode: function (nodes) {
                 nodes = $._tree.transformToArray(nodes);
                 for (var i = nodes.length - 1; i >= 0; i--) {
                     if (nodes[i].getParentNode() != null) {
@@ -98,7 +123,7 @@
                 }
             },
             // 隐藏所有节点
-            hideAllNode: function(nodes) {
+            hideAllNode: function (nodes) {
                 var tree = $.fn.zTree.getZTreeObj("tree");
                 var nodes = $._tree.transformToArray(nodes);
                 for (var i = nodes.length - 1; i >= 0; i--) {
@@ -106,86 +131,86 @@
                 }
             },
             // 显示所有父节点
-            showParent: function(treeNode) {
-                var parentNode;
-                while ((parentNode = treeNode.getParentNode()) != null) {
-                    $._tree.showNode(parentNode);
-                    $._tree.expandNode(parentNode, true, false, false);
-                    treeNode = parentNode;
-                }
-            },
+            // showParent: function(treeNode) {
+            //     var parentNode;
+            //     while ((parentNode = treeNode.getParentNode()) != null) {
+            //         $._tree.showNode(parentNode);
+            //         $._tree.expandNode(parentNode, true, false, false);
+            //         treeNode = parentNode;
+            //     }
+            // },
             // 显示所有孩子节点
-            showChildren: function(treeNode) {
-                if (treeNode.isParent) {
-                    for (var idx in treeNode.children) {
-                        var node = treeNode.children[idx];
-                        $._tree.showNode(node);
-                        $.tree.showChildren(node);
-                    }
-                }
-            },
+            // showChildren: function(treeNode) {
+            //     if (treeNode.isParent) {
+            //         for (var idx in treeNode.children) {
+            //             var node = treeNode.children[idx];
+            //             $._tree.showNode(node);
+            //             $.tree.showChildren(node);
+            //         }
+            //     }
+            // },
             // 更新节点状态
-            updateNodes: function(nodeList) {
-                $._tree.showNodes(nodeList);
-                for (var i = 0, l = nodeList.length; i < l; i++) {
-                    var treeNode = nodeList[i];
-                    $.tree.showChildren(treeNode);
-                    $.tree.showParent(treeNode)
-                }
-            },
+            // updateNodes: function(nodeList) {
+            //     $._tree.showNodes(nodeList);
+            //     for (var i = 0, l = nodeList.length; i < l; i++) {
+            //         var treeNode = nodeList[i];
+            //         $.tree.showChildren(treeNode);
+            //         $.tree.showParent(treeNode)
+            //     }
+            // },
             // 获取当前被勾选集合
-            getCheckedNodes: function(column) {
-                var _column = $.common.isEmpty(column) ? "id" : column;
-                var nodes = $._tree.getCheckedNodes(true);
-                return $.map(nodes, function (row) {
-                    return row[_column];
-                }).join();
-            },
+            // getCheckedNodes: function(column) {
+            //     var _column = $.common.isEmpty(column) ? "id" : column;
+            //     var nodes = $._tree.getCheckedNodes(true);
+            //     return $.map(nodes, function (row) {
+            //         return row[_column];
+            //     }).join();
+            // },
             // 不允许根父节点选择
-            notAllowParents: function(_tree) {
-                var nodes = _tree.getSelectedNodes();
-                if(nodes.length == 0){
-                    $.modal.msgError("请选择节点后提交");
-                    return false;
-                }
-                for (var i = 0; i < nodes.length; i++) {
-                    if (nodes[i].level == 0) {
-                        $.modal.msgError("不能选择根节点（" + nodes[i].name + "）");
-                        return false;
-                    }
-                    if (nodes[i].isParent) {
-                        $.modal.msgError("不能选择父节点（" + nodes[i].name + "）");
-                        return false;
-                    }
-                }
-                return true;
-            },
+            // notAllowParents: function(_tree) {
+            //     var nodes = _tree.getSelectedNodes();
+            //     if(nodes.length == 0){
+            //         $.modal.msgError("请选择节点后提交");
+            //         return false;
+            //     }
+            //     for (var i = 0; i < nodes.length; i++) {
+            //         if (nodes[i].level == 0) {
+            //             $.modal.msgError("不能选择根节点（" + nodes[i].name + "）");
+            //             return false;
+            //         }
+            //         if (nodes[i].isParent) {
+            //             $.modal.msgError("不能选择父节点（" + nodes[i].name + "）");
+            //             return false;
+            //         }
+            //     }
+            //     return true;
+            // },
             // 不允许最后层级节点选择
-            notAllowLastLevel: function(_tree) {
-                var nodes = _tree.getSelectedNodes();
-                for (var i = 0; i < nodes.length; i++) {
-                    if (!nodes[i].isParent) {
-                        $.modal.msgError("不能选择最后层级节点（" + nodes[i].name + "）");
-                        return false;
-                    }
-                }
-                return true;
-            },
+            // notAllowLastLevel: function(_tree) {
+            //     var nodes = _tree.getSelectedNodes();
+            //     for (var i = 0; i < nodes.length; i++) {
+            //         if (!nodes[i].isParent) {
+            //             $.modal.msgError("不能选择最后层级节点（" + nodes[i].name + "）");
+            //             return false;
+            //         }
+            //     }
+            //     return true;
+            // },
             // 隐藏/显示搜索栏
-            toggleSearch: function() {
-                $('#search').slideToggle(200);
-                $('#btnShow').toggle();
-                $('#btnHide').toggle();
-                $('#keyword').focus();
-            },
+            // toggleSearch: function() {
+            //     $('#search').slideToggle(200);
+            //     $('#btnShow').toggle();
+            //     $('#btnHide').toggle();
+            //     $('#keyword').focus();
+            // },
             // 折叠
-            collapse: function() {
-                $._tree.expandAll(false);
-            },
+            // collapse: function() {
+            //     $._tree.expandAll(false);
+            // },
             // 展开
-            expand: function() {
-                $._tree.expandAll(true);
-            }
+            // expand: function() {
+            //     $._tree.expandAll(true);
+            // }
         },
     });
 })(jQuery);
