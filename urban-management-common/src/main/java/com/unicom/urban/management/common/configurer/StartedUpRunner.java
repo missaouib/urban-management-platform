2348e21389462a126ad09385e6cdd839bad73710
liukai <zhizhufan@foxmail.com>
2020-11-04 18:27:16 +0800

    feat: application.name

---
+++ urban-management-common/src/main/java/com/unicom/urban/management/common/configurer/StartedUpRunner.java
@@ -0,0 +1,38 @@
+package com.unicom.urban.management.common.configurer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * Springboot启动之后回调
+ *
+ * @author liukai
+ */
+@Slf4j
+@Component
+public class StartedUpRunner implements ApplicationRunner {
+
+    @Autowired
+    private ConfigurableApplicationContext context;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        if (context.isActive()) {
+            log.info("  _   _   _   _   _   _ ");
+            log.info(" / \\ / \\ / \\ / \\ / \\ / \\");
+            log.info("( u | n | i | c | o | m )");
+            log.info(" \\_/ \\_/ \\_/ \\_/ \\_/ \\_/");
+            log.info("{} 启动完毕，时间：{}", applicationName, LocalDateTime.now());
+        }
+    }
+}
