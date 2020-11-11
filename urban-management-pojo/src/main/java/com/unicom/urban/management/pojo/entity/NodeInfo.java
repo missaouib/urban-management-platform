3cb39feda2ad5f5b50050ac69584407e9927c750
liukai <zhizhufan@foxmail.com>
2020-11-11 16:43:33 +0800

    feat: 增加环节信息实体类

---
+++ urban-management-pojo/src/main/java/com/unicom/urban/management/pojo/entity/NodeInfo.java
@@ -0,0 +1,31 @@
+package com.unicom.urban.management.pojo.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * 环节信息实体类
+ *
+ * @author liukai
+ */
+@Setter
+@Getter
+@Entity
+public class NodeInfo {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    private String taskName;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+
+}
