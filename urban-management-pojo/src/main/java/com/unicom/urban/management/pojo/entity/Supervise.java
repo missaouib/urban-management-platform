18b9981b64742d693bb764ac43f356681f7b558a
liukai <zhizhufan@foxmail.com>
2020-11-16 11:09:05 +0800

    feat: 督办实体类

---
+++ urban-management-pojo/src/main/java/com/unicom/urban/management/pojo/entity/Supervise.java
@@ -0,0 +1,55 @@
+package com.unicom.urban.management.pojo.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * 督办实体类
+ *
+ * @author liukai
+ */
+@Setter
+@Getter
+@Entity
+public class Supervise extends BaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    /**
+     * 督办人
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+    /**
+     * 督办部门
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dept_id")
+    private Dept dept;
+
+    /**
+     * 督办意见
+     */
+    private String opinion;
+
+    /**
+     * 回复意见
+     */
+    private String replayOpinion;
+
+    @Column(columnDefinition = "TINYINT(1)")
+    private Integer sts;
+
+}
