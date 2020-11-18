e66fce572cf045d80bb17ac6270108fbfae9acf2
顾志杰 <819162954@qq.com>
2020-11-18 15:01:01 +0800

    feat: 专业部门评价

--- urban-management-service/src/main/java/com/unicom/urban/management/service/deptevaluate/DeptEvaluateService.java
+++ urban-management-service/src/main/java/com/unicom/urban/management/service/deptevaluate/DeptEvaluateService.java
@@ -64,19 +64,19 @@ public class DeptEvaluateService {
             /*应结案数*/
             Integer mustCloseNum = this.mustCloseNum(all, d.getId());
             deptEvaluate.setMustCloseNum(mustCloseNum);
-            deptEvaluate.setCloseRate(this.getRate(closeNum,mustCloseNum));
+            deptEvaluate.setCloseRate(this.getRate(closeNum, mustCloseNum));
             deptEvaluate.setManagementNum(this.managementNum(disposeList, d.getId()));
             /*按时处置数*/
             Integer onTimeManagementNum = this.onTimeManagementNum(disposeList, d.getId());
             deptEvaluate.setOnTimeManagementNum(onTimeManagementNum);
             /*应处置数*/
             Integer mustManagementNum = this.mustManagementNum(needDisposeList, d.getId());
-            deptEvaluate.setOnTimeManagementRate(this.getRate(onTimeManagementNum,mustManagementNum));
+            deptEvaluate.setOnTimeManagementRate(this.getRate(onTimeManagementNum, mustManagementNum));
             deptEvaluate.setMustManagementNum(mustManagementNum);
             /*返工数*/
             Integer reworkNum = this.reworkNum(reworkList, d.getId());
             deptEvaluate.setReworkNum(reworkNum);
-            deptEvaluate.setReworkRate(this.getRate(reworkNum,mustCloseNum));
+            deptEvaluate.setReworkRate(this.getRate(reworkNum, mustCloseNum));
 
             list.add(deptEvaluate);
 
@@ -84,28 +84,28 @@ public class DeptEvaluateService {
         return list;
     }
 
-    private double comprehensive(Integer registerNum){
+    private double comprehensive(Integer registerNum) {
         Double register = 0.0;
-        if(registerNum <=50){
+        if (registerNum <= 50) {
             register = 10.0;
-        }else if(registerNum <=100){
+        } else if (registerNum <= 100) {
             register = 9.0;
-        }else if(registerNum <=200){
+        } else if (registerNum <= 200) {
             register = 7.5;
-        }else if(registerNum <=500){
+        } else if (registerNum <= 500) {
             register = 6.0;
-        }else if(registerNum <=1000){
+        } else if (registerNum <= 1000) {
             register = 4.0;
-        }else{
+        } else {
             register = 0.0;
         }
         return 0.0;
     }
 
 
-    private String getRate(Integer num1,Integer num2){
-        Double closeRate = (double) (num1) /(double) num2;
-        return Optional.of(nt.format(closeRate)).filter(s->!"NaN".equals(s)).orElse("0");
+    private String getRate(Integer num1, Integer num2) {
+        Double closeRate = (double) (num1) / (double) num2;
+        return Optional.of(nt.format(closeRate)).filter(s -> !"NaN".equals(s)).orElse("0");
     }
 
     /**
@@ -117,7 +117,7 @@ public class DeptEvaluateService {
         collect.forEach(s -> events.add(s.getEvent()));
         List<Statistics> statistics1 = statisticsRepository.findAllByEventInAndTaskNameAndDisposeUnit_Id(events, "专业部门", deptId);
         Integer count = Math.toIntExact(statistics1.stream().filter(s -> s.getEndTime() == null).count());
-        Integer count1 = Math.toIntExact(statistics1.stream().filter(s -> s.getDispose() == 1).count());
+        Integer count1 = Math.toIntExact(statistics1.stream().filter(s -> null != s.getDispose()).filter(s -> s.getDispose() == 1).count());
         return count + count1;
     }
 
@@ -125,7 +125,7 @@ public class DeptEvaluateService {
      * 按时结案数
      */
     private Integer onTimeCloseNum(List<Statistics> statistics, String deptId) {
-        List<Statistics> collect = statistics.stream().filter(s -> s.getInTimeClose() == 1).collect(Collectors.toList());
+        List<Statistics> collect = statistics.stream().filter(s -> null != s.getInTimeClose()).filter(s -> s.getInTimeClose() == 1).collect(Collectors.toList());
         List<Event> events = new ArrayList<>();
         collect.forEach(s -> events.add(s.getEvent()));
         return statisticsRepository.findAllByEventInAndTaskNameAndDisposeUnit_Id(events, "专业部门", deptId).size();
@@ -146,7 +146,7 @@ public class DeptEvaluateService {
      */
     private Integer mustCloseNum(List<Statistics> statistics, String deptId) {
         //TODO 应结案数 = 处置数 + 超时未处置数
-        List<Statistics> dispose = statistics.stream().filter(s->null!=s.getDispose()).filter(s -> s.getDispose() == 1).collect(Collectors.toList());
+        List<Statistics> dispose = statistics.stream().filter(s -> null != s.getDispose()).filter(s -> s.getDispose() == 1).collect(Collectors.toList());
         // 处置数
         long count = dispose.stream().filter(s -> deptId.equals(s.getDisposeUnit().getId())).count();
         return Math.toIntExact(count);
