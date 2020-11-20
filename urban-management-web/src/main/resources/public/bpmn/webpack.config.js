cb1bc0924496379563ba92c74a29c28783ade9ef
liukai <zhizhufan@foxmail.com>
2020-11-20 21:19:26 +0800

    feat: bpmn

---
+++ urban-management-web/src/main/resources/public/bpmn/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
