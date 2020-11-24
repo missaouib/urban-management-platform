8b031e996430871f30ec5be1152ac36f8a6972c7
刘博智 <lboz@163.com>
2020-11-24 18:31:11 +0800

    feat:岗位评价

--- urban-management-dao/src/main/java/com/unicom/urban/management/dao/evaluate/PositionRepository.java
+++ urban-management-dao/src/main/java/com/unicom/urban/management/dao/evaluate/PositionRepository.java
@@ -40,12 +40,12 @@ public interface PositionRepository extends CustomizeRepository<Statistics, Loca
             "\t\t\t\t\t\t#漏报率：漏报数/立案数×100%\n" +
             "            (sum(public_report)/sum(inst)) as publicReportRate,\n" +
             "\t\t\t\t\t\t#综合指标值=监督员有效上报率分值×40%+漏报率分值×20%+按时核实率分值×20%+按时核查率分值×20%\n" +
-            "            ((sum(valid_patrol_report)*100*0.4+(1-(sum(public_report)/sum(inst)))*100*0.2+(sum(in_time_verify)/sum(need_verify))*100*0.2)+ (sum(in_time_check)/sum(need_send_check))*100*0.2) as aggregativeIndicator,\n" +
+            "            ((sum(valid_patrol_report)*0.4+(1-(sum(public_report)/sum(inst)))*0.2+(sum(in_time_verify)/sum(need_verify))*0.2)+ (sum(in_time_check)/sum(need_send_check))*0.2) as aggregativeIndicator,\n" +
             "            (CASE \n" +
-            "            WHEN 90<=((sum(valid_patrol_report)*100*0.4+(1-(sum(public_report)/sum(inst)))*100*0.2+(sum(in_time_verify)/sum(need_verify))*100*0.2)+ (sum(in_time_check)/sum(need_send_check))*100*0.2)<=100 THEN 'A'\n" +
-            "            WHEN 75<=((sum(valid_patrol_report)*100*0.4+(1-(sum(public_report)/sum(inst)))*100*0.2+(sum(in_time_verify)/sum(need_verify))*100*0.2)+ (sum(in_time_check)/sum(need_send_check))*100*0.2)<90 THEN 'B'\n" +
-            "            WHEN 60<=((sum(valid_patrol_report)*100*0.4+(1-(sum(public_report)/sum(inst)))*100*0.2+(sum(in_time_verify)/sum(need_verify))*100*0.2)+ (sum(in_time_check)/sum(need_send_check))*100*0.2)<75 THEN 'C'\n" +
-            "            WHEN 40<=((sum(valid_patrol_report)*100*0.4+(1-(sum(public_report)/sum(inst)))*100*0.2+(sum(in_time_verify)/sum(need_verify))*100*0.2)+ (sum(in_time_check)/sum(need_send_check))*100*0.2)<60 THEN 'D'\n" +
+            "            WHEN 90<=((sum(valid_patrol_report)*0.4+(1-(sum(public_report)/sum(inst)))*0.2+(sum(in_time_verify)/sum(need_verify))*0.2)+ (sum(in_time_check)/sum(need_send_check))*0.2)<=100 THEN 'A'\n" +
+            "            WHEN 75<=((sum(valid_patrol_report)*0.4+(1-(sum(public_report)/sum(inst)))*0.2+(sum(in_time_verify)/sum(need_verify))*0.2)+ (sum(in_time_check)/sum(need_send_check))*0.2)<90 THEN 'B'\n" +
+            "            WHEN 60<=((sum(valid_patrol_report)*0.4+(1-(sum(public_report)/sum(inst)))*0.2+(sum(in_time_verify)/sum(need_verify))*0.2)+ (sum(in_time_check)/sum(need_send_check))*0.2)<75 THEN 'C'\n" +
+            "            WHEN 40<=((sum(valid_patrol_report)*0.4+(1-(sum(public_report)/sum(inst)))*0.2+(sum(in_time_verify)/sum(need_verify))*0.2)+ (sum(in_time_check)/sum(need_send_check))*0.2)<60 THEN 'D'\n" +
             "             ELSE 'E' END ) as ratingLevel\n" +
             "            from grid g,statistics st,`event` ev,sys_user su where  \n" +
             "\t\t\t\t\t\t (su.id=st.report_patrol_name_id or su.id=st.inst_human_name_id or su.id=st.check_patrol_name) \n" +
@@ -62,7 +62,7 @@ public interface PositionRepository extends CustomizeRepository<Statistics, Loca
             "(sum(in_time_send_verify)/sum(need_send_verify)) as sendVerifyRate,#核实按时派发率\n" +
             "sum(in_time_send_check) as intimeSendCheck,sum(need_send_check) as needSendCheck,\n" +
             "(sum(in_time_send_check)/sum(need_send_check)) as needSendCheckRate,#核查按时派发率\n" +
-            "(sum(operate)*0.25+(sum(in_time_send_verify)/sum(need_send_verify))*100*0.4+(sum(in_time_send_check)/sum(need_send_check))*100*0.35) as aggregativeIndicator,#综合指标值=受理数分值×25%+核实按时派发率分值×40%+核查按时派发率分值×35%\n" +
+            "(sum(operate)*0.25+(sum(in_time_send_verify)/sum(need_send_verify))*0.4+(sum(in_time_send_check)/sum(need_send_check))*0.35) as aggregativeIndicator,#综合指标值=受理数分值×25%+核实按时派发率分值×40%+核查按时派发率分值×35%\n" +
             "(CASE \n" +
             "\tWHEN 90<=((CASE \n" +
             "\tWHEN sum(operate) > 2001 THEN 100\n" +
@@ -70,28 +70,28 @@ public interface PositionRepository extends CustomizeRepository<Statistics, Loca
             "\t\tWHEN 501<=sum(operate) <= 1200 THEN 75\n" +
             "\t\tWHEN 301<=sum(operate) <= 500 THEN 60\n" +
             "\t\tWHEN 100<=sum(operate)THEN 0\n" +
-            "END)*0.25+(sum(in_time_send_verify)/sum(need_send_verify))*100*0.4+(sum(in_time_send_check)/sum(need_send_check))*100*0.35)<=100 THEN 'A'\t\n" +
+            "END)*0.25+(sum(in_time_send_verify)/sum(need_send_verify))*0.4+(sum(in_time_send_check)/sum(need_send_check))*0.35)<=100 THEN 'A'\t\n" +
             "\tWHEN 75<=((CASE \n" +
             "\tWHEN sum(operate) > 2001 THEN 100\n" +
             "\t\tWHEN 1201<=sum(operate) <= 2000 THEN 90\n" +
             "\t\tWHEN 501<=sum(operate) <= 1200 THEN 75\n" +
             "\t\tWHEN 301<=sum(operate) <= 500 THEN 60\n" +
             "\t\tWHEN 100<=sum(operate)THEN 0\n" +
-            "END)*0.25+(sum(in_time_send_verify)/sum(need_send_verify))*100*0.4+(sum(in_time_send_check)/sum(need_send_check))*100*0.35)<90 THEN 'B'\n" +
+            "END)*0.25+(sum(in_time_send_verify)/sum(need_send_verify))*0.4+(sum(in_time_send_check)/sum(need_send_check))*0.35)<90 THEN 'B'\n" +
             "\tWHEN 60<=((CASE \n" +
             "\tWHEN sum(operate) > 2001 THEN 100\n" +
             "\t\tWHEN 1201<=sum(operate) <= 2000 THEN 90\n" +
             "\t\tWHEN 501<=sum(operate) <= 1200 THEN 75\n" +
             "\t\tWHEN 301<=sum(operate) <= 500 THEN 60\n" +
             "\t\tWHEN 100<=sum(operate)THEN 0\n" +
-            "END)*0.25+(sum(in_time_send_verify)/sum(need_send_verify))*100*0.4+(sum(in_time_send_check)/sum(need_send_check))*100*0.35)<75 THEN 'C'\n" +
+            "END)*0.25+(sum(in_time_send_verify)/sum(need_send_verify))*0.4+(sum(in_time_send_check)/sum(need_send_check))*0.35)<75 THEN 'C'\n" +
             "\tWHEN 40<=((CASE \n" +
             "\tWHEN sum(operate) > 2001 THEN 100\n" +
             "\t\tWHEN 1201<=sum(operate) <= 2000 THEN 90\n" +
             "\t\tWHEN 501<=sum(operate) <= 1200 THEN 75\n" +
             "\t\tWHEN 301<=sum(operate) <= 500 THEN 60\n" +
             "\t\tWHEN 100<=sum(operate)THEN 0\n" +
-            "END)*0.25+(sum(in_time_send_verify)/sum(need_send_verify))*100*0.4+(sum(in_time_send_check)/sum(need_send_check))*100*0.35)<60 THEN 'D'\n" +
+            "END)*0.25+(sum(in_time_send_verify)/sum(need_send_verify))*0.4+(sum(in_time_send_check)/sum(need_send_check))*0.35)<60 THEN 'D'\n" +
             "\tELSE 'E' END\n" +
             ")as ratingLevel #评价等级\n" +
             "from  statistics st,sys_user su,`event` ev where st.event_id=ev.id and  (su.id=st.operate_human_id or su.id=st.send_check_human_name) and ev.create_time between ?1 and ?2 GROUP BY su.`name`", nativeQuery = true)
@@ -120,12 +120,12 @@ public interface PositionRepository extends CustomizeRepository<Statistics, Loca
             "#按时结案率 按时结案数/结案数×100%。\n" +
             " sum(in_time_close)/sum(`close`) as inTimeCloseRate,\n" +
             " #综合指标值 = 按时立案率分值×25%+准确立案率分值×40%+按时结案率分值×35%\n" +
-            " ((sum(in_time_close)/sum(`close`))*100*0.25+((sum(inst)-ifnull(sum(cancel),0))/sum(inst))*100*0.4+(sum(in_time_close)/sum(`close`))*100*0.35) as aggregativeIndicator,\n" +
+            " ((sum(in_time_close)/sum(`close`))*0.25+((sum(inst)-ifnull(sum(cancel),0))/sum(inst))*0.4+(sum(in_time_close)/sum(`close`))*0.35) as aggregativeIndicator,\n" +
             "(CASE \n" +
-            "\tWHEN 90<=((sum(in_time_close)/sum(`close`))*100*0.25+((sum(inst)-ifnull(sum(cancel),0))/sum(inst))*100*0.4+(sum(in_time_close)/sum(`close`))*100*0.35)<=100 THEN 'A'\t\n" +
-            "\tWHEN 75<=((sum(in_time_close)/sum(`close`))*100*0.25+((sum(inst)-ifnull(sum(cancel),0))/sum(inst))*100*0.4+(sum(in_time_close)/sum(`close`))*100*0.35)<90 THEN 'B'\n" +
-            "\tWHEN 60<=((sum(in_time_close)/sum(`close`))*100*0.25+((sum(inst)-ifnull(sum(cancel),0))/sum(inst))*100*0.4+(sum(in_time_close)/sum(`close`))*100*0.35)<75 THEN 'C'\n" +
-            "\tWHEN 40<=((sum(in_time_close)/sum(`close`))*100*0.25+((sum(inst)-ifnull(sum(cancel),0))/sum(inst))*100*0.4+(sum(in_time_close)/sum(`close`))*100*0.35)<60 THEN 'D'\n" +
+            "\tWHEN 90<=((sum(in_time_close)/sum(`close`))*0.25+((sum(inst)-ifnull(sum(cancel),0))/sum(inst))*0.4+(sum(in_time_close)/sum(`close`))*0.35)<=100 THEN 'A'\t\n" +
+            "\tWHEN 75<=((sum(in_time_close)/sum(`close`))*0.25+((sum(inst)-ifnull(sum(cancel),0))/sum(inst))*0.4+(sum(in_time_close)/sum(`close`))*0.35)<90 THEN 'B'\n" +
+            "\tWHEN 60<=((sum(in_time_close)/sum(`close`))*0.25+((sum(inst)-ifnull(sum(cancel),0))/sum(inst))*0.4+(sum(in_time_close)/sum(`close`))*0.35)<75 THEN 'C'\n" +
+            "\tWHEN 40<=((sum(in_time_close)/sum(`close`))*0.25+((sum(inst)-ifnull(sum(cancel),0))/sum(inst))*0.4+(sum(in_time_close)/sum(`close`))*0.35)<60 THEN 'D'\n" +
             "\tELSE 'E' END\n" +
             ") as ratingLevel#评价等级 \n" +
             "from  statistics st,sys_user su,`event` ev where (st.inst_human_name_id=su.id or st.close_human_name_id=su.id) and st.event_id=ev.id and ev.create_time between ?1 and ?2 GROUP BY su.name\n", nativeQuery = true)
@@ -150,13 +150,13 @@ public interface PositionRepository extends CustomizeRepository<Statistics, Loca
             "#准确派遣率：准确派遣数/应派遣数×100%\n" +
             "(sum(need_dispatch)-ifnull(sum(rework),0))/sum(in_time_dispatch) as accuracyDispatchRate,\n" +
             "#综合指标值=派遣数分值×20%+按时派遣率×40%+准确派遣率分值×40%\n" +
-            " (sum(to_dispatch)*100*0.2+(sum(in_time_dispatch)/sum(need_dispatch))*100*0.4+(sum(need_dispatch)-ifnull(sum(rework),0))/sum(in_time_dispatch)*100*0.4) as aggregativeIndicator,\n" +
+            " (sum(to_dispatch)*0.2+(sum(in_time_dispatch)/sum(need_dispatch))*0.4+(sum(need_dispatch)-ifnull(sum(rework),0))/sum(in_time_dispatch)*0.4) as aggregativeIndicator,\n" +
             "#评价等级\n" +
             "(CASE \n" +
-            "\tWHEN 90<=(sum(to_dispatch)*100*0.2+(sum(in_time_dispatch)/sum(need_dispatch))*100*0.4+(sum(accuracy_dispatch)/sum(in_time_dispatch))*100*0.4)<=100 THEN 'A'\t\n" +
-            "\tWHEN 75<=(sum(to_dispatch)*100*0.2+(sum(in_time_dispatch)/sum(need_dispatch))*100*0.4+(sum(accuracy_dispatch)/sum(in_time_dispatch))*100*0.4)<90 THEN 'B'\n" +
-            "\tWHEN 60<=(sum(to_dispatch)*100*0.2+(sum(in_time_dispatch)/sum(need_dispatch))*100*0.4+(sum(accuracy_dispatch)/sum(in_time_dispatch))*100*0.4)<75 THEN 'C'\n" +
-            "\tWHEN 40<=(sum(to_dispatch)*100*0.2+(sum(in_time_dispatch)/sum(need_dispatch))*100*0.4+(sum(accuracy_dispatch)/sum(in_time_dispatch))*100*0.4)<60 THEN 'D'\n" +
+            "\tWHEN 90<=(sum(to_dispatch)*0.2+(sum(in_time_dispatch)/sum(need_dispatch))*0.4+(sum(accuracy_dispatch)/sum(in_time_dispatch))*0.4)<=100 THEN 'A'\t\n" +
+            "\tWHEN 75<=(sum(to_dispatch)*0.2+(sum(in_time_dispatch)/sum(need_dispatch))*0.4+(sum(accuracy_dispatch)/sum(in_time_dispatch))*0.4)<90 THEN 'B'\n" +
+            "\tWHEN 60<=(sum(to_dispatch)*0.2+(sum(in_time_dispatch)/sum(need_dispatch))*0.4+(sum(accuracy_dispatch)/sum(in_time_dispatch))*0.4)<75 THEN 'C'\n" +
+            "\tWHEN 40<=(sum(to_dispatch)*0.2+(sum(in_time_dispatch)/sum(need_dispatch))*0.4+(sum(accuracy_dispatch)/sum(in_time_dispatch))*0.4)<60 THEN 'D'\n" +
             "\tELSE 'E' END\n" +
             ") as ratingLevel\n" +
             "from  statistics st,sys_user su,`event` ev where dispatch is not null and st.dispatch_human_name_id=su.id and st.event_id=ev.id and ev.create_time between ?1 and ?2 GROUP BY su.name", nativeQuery = true)
