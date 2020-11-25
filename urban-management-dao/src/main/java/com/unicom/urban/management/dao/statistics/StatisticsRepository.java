6261863718f60832b42532be7a247659f1a9c36b
jiangw <373438073@qq.com>
2020-11-25 19:50:21 +0800

    feat: 首页echarts渲染
    首页echarts渲染

--- urban-management-dao/src/main/java/com/unicom/urban/management/dao/statistics/StatisticsRepository.java
+++ urban-management-dao/src/main/java/com/unicom/urban/management/dao/statistics/StatisticsRepository.java
@@ -64,7 +64,7 @@ public interface StatisticsRepository extends CustomizeRepository<Statistics, St
      * @param gridId 网格id
      * @return 数据
      */
-    @Query(value = "SELECT s,e,g FROM Statistics s LEFT JOIN Event e on s.event = e.id LEFT JOIN e.grid g on e.grid.id = g.id WHERE s.inTimeClose= 1 and g.id = ?1 and e.createTime between ?2 and ?3 ")
+    @Query(value = "SELECT s,e,g FROM Statistics s LEFT JOIN s.event e LEFT JOIN e.grid g WHERE s.inTimeClose= 1 and g.id = ?1 and e.createTime between ?2 and ?3 ")
     List<Statistics> findAllByInTimeClose(String gridId, LocalDateTime startTime, LocalDateTime endTime);
 
 
@@ -77,7 +77,7 @@ public interface StatisticsRepository extends CustomizeRepository<Statistics, St
      * @param gridId 网格id
      * @return 数据
      */
-    @Query(value = "SELECT s,e,g FROM Statistics s LEFT JOIN Event e on s.event = e.id LEFT JOIN e.grid g on e.grid.id = g.id WHERE s.close = 1 and g.id = ?1 and e.createTime between ?2 and ?3 ")
+    @Query(value = "SELECT s,e,g FROM Statistics s LEFT JOIN s.event e LEFT JOIN e.grid g WHERE s.close = 1 and g.id = ?1 and e.createTime between ?2 and ?3 ")
     List<Statistics> findAllByClose(String gridId, LocalDateTime startTime, LocalDateTime endTime);
 
     /**
@@ -86,7 +86,7 @@ public interface StatisticsRepository extends CustomizeRepository<Statistics, St
      * @param gridId 网格id
      * @return 数据
      */
-    @Query(value = "SELECT s,e,g FROM Statistics s LEFT JOIN Event e on s.event = e.id LEFT JOIN e.grid g on e.grid.id = g.id WHERE (s.close = 1 or s.toClose = 1) and g.id = ?1 and e.createTime between ?2 and ?3 ")
+    @Query(value = "SELECT s,e,g FROM Statistics s LEFT JOIN s.event e LEFT JOIN e.grid g WHERE (s.close = 1 or s.toClose = 1) and g.id = ?1 and e.createTime between ?2 and ?3 ")
     List<Statistics> findAllByCloseOrToClose(String gridId, LocalDateTime startTime, LocalDateTime endTime);
 
     /**
@@ -95,7 +95,7 @@ public interface StatisticsRepository extends CustomizeRepository<Statistics, St
      * @param gridId 网格id
      * @return 数据
      */
-    @Query(value = "SELECT s,e,g FROM Statistics s LEFT JOIN Event e on s.event = e.id LEFT JOIN e.grid g on e.grid.id = g.id WHERE s.event.id in (SELECT ss.event.id FROM Statistics ss where ss.publicReport = 1) and s.inst = 1 and g.id = ?1 and e.createTime between ?2 and ?3 ")
+    @Query(value = "SELECT s,e,g FROM Statistics s LEFT JOIN s.event e LEFT JOIN e.grid g WHERE s.event.id in (SELECT ss.event.id FROM Statistics ss where ss.publicReport = 1) and s.inst = 1 and g.id = ?1 and e.createTime between ?2 and ?3 ")
     List<Statistics> findAllByPublicReportAndInst(String gridId, LocalDateTime startTime, LocalDateTime endTime);
 
     /**
@@ -104,7 +104,7 @@ public interface StatisticsRepository extends CustomizeRepository<Statistics, St
      * @param gridId 网格id
      * @return 数据
      */
-    @Query(value = "SELECT s,e,g FROM Statistics s LEFT JOIN Event e on s.event = e.id LEFT JOIN e.grid g on e.grid.id = g.id WHERE s.inst = 1 and g.id = ?1 and e.createTime between ?2 and ?3 ")
+    @Query(value = "SELECT s,e,g FROM Statistics s LEFT JOIN s.event e LEFT JOIN e.grid g WHERE s.inst = 1 and g.id = ?1 and e.createTime between ?2 and ?3 ")
     List<Statistics> findAllByInst(String gridId, LocalDateTime startTime, LocalDateTime endTime);
 
     /**
@@ -113,7 +113,7 @@ public interface StatisticsRepository extends CustomizeRepository<Statistics, St
      * @param gridId 网格id
      * @return 数据
      */
-    @Query(value = "SELECT s,e,g FROM Statistics s LEFT JOIN Event e on s.event = e.id LEFT JOIN e.grid g on e.grid.id = g.id WHERE s.hang = 1 and g.id = ?1 ")
+    @Query(value = "SELECT s,e,g FROM Statistics s LEFT JOIN s.event e LEFT JOIN e.grid g WHERE s.hang = 1 and g.id = ?1 ")
     List<Statistics> findAllByHang(String gridId);
 
     /**
@@ -134,7 +134,7 @@ public interface StatisticsRepository extends CustomizeRepository<Statistics, St
      * @param endTime   结束时间
      * @return list
      */
-    @Query(value = "SELECT s,e FROM Statistics s LEFT JOIN Event e on s.event = e.id WHERE s.report = 1 and e.createTime between ?1 and ?2 ")
+    @Query(value = "SELECT s,e FROM Statistics s LEFT JOIN s.event e WHERE s.report = 1 and e.createTime between ?1 and ?2 ")
     List<Statistics> findByReport(LocalDateTime startTime, LocalDateTime endTime);
 
     /**
@@ -144,7 +144,7 @@ public interface StatisticsRepository extends CustomizeRepository<Statistics, St
      * @param endTime   结束时间
      * @return list
      */
-    @Query(value = "SELECT s,e FROM Statistics s LEFT JOIN Event e on s.event = e.id WHERE s.operate = 1 and e.createTime between ?1 and ?2 ")
+    @Query(value = "SELECT s,e FROM Statistics s LEFT JOIN s.event e WHERE s.operate = 1 and e.createTime between ?1 and ?2 ")
     List<Statistics> findByOperate(LocalDateTime startTime, LocalDateTime endTime);
 
     /**
@@ -154,7 +154,7 @@ public interface StatisticsRepository extends CustomizeRepository<Statistics, St
      * @param endTime   结束时间
      * @return list
      */
-    @Query(value = "SELECT s,e FROM Statistics s LEFT JOIN Event e on s.event = e.id WHERE s.inst = 1 and e.createTime between ?1 and ?2 ")
+    @Query(value = "SELECT s,e FROM Statistics s LEFT JOIN s.event e WHERE s.inst = 1 and e.createTime between ?1 and ?2 ")
     List<Statistics> findByInst(LocalDateTime startTime, LocalDateTime endTime);
 
     /**
@@ -164,7 +164,7 @@ public interface StatisticsRepository extends CustomizeRepository<Statistics, St
      * @param endTime   结束时间
      * @return list
      */
-    @Query(value = "SELECT s,e FROM Statistics s LEFT JOIN Event e on s.event = e.id WHERE s.dispatch = 1 and e.createTime between ?1 and ?2 ")
+    @Query(value = "SELECT s,e FROM Statistics s LEFT JOIN s.event e WHERE s.dispatch = 1 and e.createTime between ?1 and ?2 ")
     List<Statistics> findByDispatch(LocalDateTime startTime, LocalDateTime endTime);
 
     /**
@@ -174,7 +174,7 @@ public interface StatisticsRepository extends CustomizeRepository<Statistics, St
      * @param endTime   结束时间
      * @return list
      */
-    @Query(value = "SELECT s,e FROM Statistics s LEFT JOIN Event e on s.event = e.id WHERE s.dispose = 1 and e.createTime between ?1 and ?2 ")
+    @Query(value = "SELECT s,e FROM Statistics s LEFT JOIN s.event e WHERE s.dispose = 1 and e.createTime between ?1 and ?2 ")
     List<Statistics> findByDispose(LocalDateTime startTime, LocalDateTime endTime);
 
     /**
@@ -184,7 +184,7 @@ public interface StatisticsRepository extends CustomizeRepository<Statistics, St
      * @param endTime   结束时间
      * @return list
      */
-    @Query(value = "SELECT s,e FROM Statistics s LEFT JOIN Event e on s.event = e.id WHERE s.checkNum = 1 and e.createTime between ?1 and ?2 ")
+    @Query(value = "SELECT s,e FROM Statistics s LEFT JOIN s.event e WHERE s.checkNum = 1 and e.createTime between ?1 and ?2 ")
     List<Statistics> findByCheckNum(LocalDateTime startTime, LocalDateTime endTime);
 
     /**
@@ -194,15 +194,19 @@ public interface StatisticsRepository extends CustomizeRepository<Statistics, St
      * @param endTime   结束时间
      * @return list
      */
-    @Query(value = "SELECT s,e FROM Statistics s LEFT JOIN Event e on s.event = e.id WHERE s.close = 1 and e.createTime between ?1 and ?2 ")
+    @Query(value = "SELECT s,e FROM Statistics s LEFT JOIN s.event e WHERE s.close = 1 and e.createTime between ?1 and ?2 ")
     List<Statistics> findByClose(LocalDateTime startTime, LocalDateTime endTime);
+
+    /* -------------------------------------------------------------- */
+
     /**
      * 岗位案件数量
      *
      * @return int
      */
-    @Query(value = "select sum(1) from statistics where report_patrol_name_id=?1  or verify_patrol_name_id=?1  or operate_human_name_id=?1  or send_verify_human_name_id=?1  or send_check_human_name=?1  or inst_human_name_id=?1  or close_human_name_id=?1  or dispatch_human_name_id=?1  or dispose_unit_name_id=?1  or check_patrol_name=?1  or check_trans_human_name=?1 ",nativeQuery = true)
+    @Query(value = "select sum(1) from statistics where report_patrol_name_id=?1  or verify_patrol_name_id=?1  or operate_human_name_id=?1  or send_verify_human_name_id=?1  or send_check_human_name=?1  or inst_human_name_id=?1  or close_human_name_id=?1  or dispatch_human_name_id=?1  or dispose_unit_name_id=?1  or check_patrol_name=?1  or check_trans_human_name=?1 ", nativeQuery = true)
     Integer findTakeCase(String userId);
+
     /**
      * 办理案件数量
      *
@@ -222,13 +226,33 @@ public interface StatisticsRepository extends CustomizeRepository<Statistics, St
      *
      * @return int
      */
-    @Query(value = "select count(1) from `event` where event_source_id in (select id from kv where field_name='eventSource') and do_by_self is NULL ",nativeQuery = true)
+    @Query(value = "select count(1) from `event` where event_source_id in (select id from kv where field_name='eventSource') and do_by_self is NULL ", nativeQuery = true)
     Integer findReportPatrolNum();
+
     /**
      * 问题来源-自行处理上报
      *
      * @return int
      */
-    @Query(value = "select count(1) from `event` where event_source_id in (select id from kv where field_name='eventSource') and do_by_self=1",nativeQuery = true)
+    @Query(value = "select count(1) from `event` where event_source_id in (select id from kv where field_name='eventSource') and do_by_self=1", nativeQuery = true)
     Integer findReportSelfNum();
+
+    /* -------------------------------------------------------------- */
+
+    /**
+     * 高发问题 立案
+     *
+     * @return list
+     */
+    @Query(value = "SELECT count(s.id) as totalInst, et.name as name, et.id as id FROM Statistics s LEFT JOIN s.event e LEFT JOIN e.eventType et WHERE s.inst = 1 GROUP BY e.eventType")
+    List<Map<String, String>> findHighIncidenceByInst();
+
+    /**
+     * 高发问题 立案
+     *
+     * @return list
+     */
+    @Query(value = "SELECT count(s.id) as totalClose FROM Statistics s LEFT JOIN s.event e LEFT JOIN e.eventType et WHERE s.close = 1 AND et.id = ?1")
+    Map<String, String> findHighIncidenceByClose(String eventTypeId);
+
 }
