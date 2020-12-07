2c979377bc7d4902b11769d15104e622a6c5b1f7
liubozhi <lboz@163.com>
2020-12-07 14:53:15 +0800

    feat: 新增权限分类实体类

---
+++ urban-management-pojo/src/main/java/com/unicom/urban/management/pojo/entity/MenuType.java
@@ -0,0 +1,33 @@
+package com.unicom.urban.management.pojo.entity;
+
+import com.unicom.urban.management.pojo.Delete;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 权限分类实体类
+ *
+ * @author liubozhi
+ */
+@Setter
+@Getter
+@Entity
+@Table(name = "menu_type")
+public class MenuType {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    private String name;
+
+    private Integer sort;
+}
