052b4ed3af5cd89929800a78f40b3e79964e9ecd
jiangw <373438073@qq.com>
2020-12-07 14:26:18 +0800

    feat: 新增页
    新增页

---
+++ urban-management-web/src/main/java/com/unicom/urban/management/web/project/rolepermissionconfiguration/RolePermissionConfigurationController.java
@@ -0,0 +1,30 @@
+package com.unicom.urban.management.web.project.rolepermissionconfiguration;
+
+import com.unicom.urban.management.common.annotations.ResponseResultBody;
+import com.unicom.urban.management.common.constant.SystemConstant;
+import com.unicom.urban.management.service.menu.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 角色权限配置
+ *
+ * @author jiangwen
+ */
+@RestController
+@ResponseResultBody
+@RequestMapping("/rolePermissionConfiguration")
+public class RolePermissionConfigurationController {
+
+    @Autowired
+    private MenuService menuService;
+
+    @GetMapping("/index")
+    public ModelAndView index() {
+        return new ModelAndView(SystemConstant.PAGE + "/rolePermissionConfiguration/index");
+    }
+
+}
