18e6bd2c293e224c81e30f3599bf5946dacbf76e
jiangw <373438073@qq.com>
2020-12-08 14:09:52 +0800

    feat: 人员设置
    人员设置

--- urban-management-web/src/main/java/com/unicom/urban/management/web/project/usersetup/UserSetupController.java
+++ urban-management-web/src/main/java/com/unicom/urban/management/web/project/usersetup/UserSetupController.java
@@ -2,12 +2,16 @@ package com.unicom.urban.management.web.project.usersetup;
 
 import com.unicom.urban.management.common.annotations.ResponseResultBody;
 import com.unicom.urban.management.common.constant.SystemConstant;
-import com.unicom.urban.management.service.menu.MenuService;
+import com.unicom.urban.management.pojo.Result;
+import com.unicom.urban.management.pojo.vo.DeptVO;
+import com.unicom.urban.management.service.dept.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * 人员设置
@@ -20,11 +24,17 @@ import org.springframework.web.servlet.ModelAndView;
 public class UserSetupController {
 
     @Autowired
-    private MenuService menuService;
+    private DeptService deptService;
 
     @GetMapping("/index")
     public ModelAndView index() {
         return new ModelAndView(SystemConstant.PAGE + "/userSetup/index");
+    }
+
+    @GetMapping("/getAllAndUserForTree")
+    public Result getAllAndUserForTree() {
+        List<DeptVO> allAndRoleForTree = deptService.getAllAndUserForTree();
+        return Result.success(allAndRoleForTree);
     }
 
 }
