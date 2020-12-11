78e02980099c638dd5c859b6186fb8773e9fea09
jiangw <373438073@qq.com>
2020-12-11 13:42:52 +0800

    feat: 移动终端配置init
    移动终端配置init

---
+++ urban-management-web/src/main/java/com/unicom/urban/management/web/project/grid/GridOwnerController.java
@@ -0,0 +1,35 @@
+package com.unicom.urban.management.web.project.grid;
+
+import com.unicom.urban.management.common.annotations.ResponseResultBody;
+import com.unicom.urban.management.common.constant.SystemConstant;
+import com.unicom.urban.management.service.grid.GridService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 移动终端配置
+ *
+ * @author jiangwen
+ */
+@RestController
+@ResponseResultBody
+@RequestMapping("/gridOwner")
+public class GridOwnerController {
+
+    @Autowired
+    private GridService gridService;
+
+    @GetMapping("/list")
+    public ModelAndView list() {
+        return new ModelAndView(SystemConstant.PAGE + "/gridOwner/list");
+    }
+
+    @GetMapping("/add")
+    public ModelAndView add() {
+        return new ModelAndView(SystemConstant.PAGE + "/gridOwner/add");
+    }
+
+}
