a9063781754aae67e33d3260413355f13aeaf0b4
wangzhe <965270040@qq.com>
2020-12-29 15:10:10 +0800

    feat：框选提示

--- urban-management-web/src/main/resources/static/js/currency.js
+++ urban-management-web/src/main/resources/static/js/currency.js
@@ -5,3 +5,17 @@ $(".banners").on("mouseover",".roleSize",function () {
     var problemTitle = $(this).text()
     $(".roleSize").attr("title",problemTitle);
 })
+
+$(document).keydown(function(event){
+    let codeKey = event.keyCode;
+    if (codeKey == 17 || codeKey == 91){
+        $(".tips_sele").addClass('roleBox')
+    }
+});
+
+$(document).keyup(function(event){
+    let codeKey = event.keyCode;
+    if (codeKey == 17 || codeKey == 91) {
+        $(".tips_sele").removeClass('roleBox')
+    }
+});
