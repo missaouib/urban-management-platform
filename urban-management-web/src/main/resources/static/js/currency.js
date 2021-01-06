5e9c3d35972deeed7f194aa287a24f73d153d7f0
wangzhe <965270040@qq.com>
2021-01-06 10:09:40 +0800

    feat：评价按钮调整 点击本周  本季度 更改

--- urban-management-web/src/main/resources/static/js/currency.js
+++ urban-management-web/src/main/resources/static/js/currency.js
@@ -63,8 +63,57 @@ $(".fullScreen_index").click(function () {
     $.learuntab.requestFullScreen();
 })
 
+/*
+获取本周
+*/
+function getWeek() {
+    this.nowTime = new Date();
+    this.init = function() {
+        this.dayInWeek = this.nowTime.getDay();
+        this.dayInWeek == 0 && (this.dayInWeek = 7);
+        this.thsiWeekFirstDay = this.nowTime.getTime() - (this.dayInWeek - 1) * 86400000;
+        this.thisWeekLastDay = this.nowTime.getTime() + (7 - this.dayInWeek) * 86400000;
+        return this;
+    };
+    this.getWeekType = function(type) {
+        type = ~~type;
+        var firstDay = this.thsiWeekFirstDay + type * 7 * 86400000;
+        var lastDay = this.thisWeekLastDay + type * 7 * 86400000;
+        return this.getWeekHtml(firstDay, lastDay);
+    }
+    this.formateDate = function(time) {
+        var newTime = new Date(time)
+        var year = newTime.getFullYear();
+        var month = newTime.getMonth() + 1;
+        var day = newTime.getDate();
+        return year + "-" + (month >= 10 ? month : "0" + month) + "-" + (day >= 10 ? day : "0" + day);
+    };
+    this.getWeekHtml = function(f, l) {
+        return this.formateDate(f) + "," + this.formateDate(l);
+    };
+}
 
-
-
-
-
+/*
+* 本季度
+* */
+function getQuarterMonth(){
+    var now = new Date();
+    var nowMonth = now.getMonth();
+    var year = now.getFullYear();
+    var starQuarte,endQyarte;
+    if(nowMonth>= 0 && nowMonth<= 2){
+        starQuarte = year + '-01-01 00:00:00';
+        endQyarte = year + '-03-31 23:59:59'
+    }else if(nowMonth>= 3 && nowMonth<= 5){
+        starQuarte = year + '-04-01 00:00:00';
+        endQyarte = year + '-06-30 23:59:59'
+    }else if(nowMonth>= 6 && nowMonth<= 8){
+        starQuarte = year + '-07-01 00:00:00';
+        endQyarte = year + '-09-30 23:59:59'
+    }else {
+        starQuarte = year + '-10-01 00:00:00';
+        endQyarte = year + '-12-31 23:59:59'
+    }
+    $("#begin").val(starQuarte)
+    $("#end").val(endQyarte)
+}
