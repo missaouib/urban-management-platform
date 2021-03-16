60ac65f47101f3a1cb72696f442d2b7c3d9141cb
liukai <zhizhufan@foxmail.com>
2021-03-16 10:43:53 +0800

    feat: 修改日志实体

--- urban-management-pojo/src/main/java/com/unicom/urban/management/pojo/entity/LoginLog.java
+++ urban-management-pojo/src/main/java/com/unicom/urban/management/pojo/entity/LoginLog.java
@@ -19,27 +19,83 @@ import java.time.LocalDateTime;
 @Table(name = "sys_login_log")
 public class LoginLog {
 
-
-    @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
     private String id;
 
-    @Column(nullable = false)
     private String username;
 
-    @Column(nullable = false)
     private String ip;
 
     private String browser;
 
     private String os;
 
-    @Column(columnDefinition = "tinyint")
     private Integer message;
 
-    @Column(nullable = false)
     private LocalDateTime loginTime;
 
 
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Column
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Column
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    @Column
+    public String getBrowser() {
+        return browser;
+    }
+
+    public void setBrowser(String browser) {
+        this.browser = browser;
+    }
+
+    @Column
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    @Column
+    public Integer getMessage() {
+        return message;
+    }
+
+    public void setMessage(Integer message) {
+        this.message = message;
+    }
+
+    @Column
+    public LocalDateTime getLoginTime() {
+        return loginTime;
+    }
+
+    public void setLoginTime(LocalDateTime loginTime) {
+        this.loginTime = loginTime;
+    }
 }
