104012ae1935bb41256f042b1d4e3265e149d7c6
顾志杰 <819162954@qq.com>
2021-03-22 18:29:39 +0800

    feat: 上传接口

--- urban-management-api/src/main/java/com/unicom/urban/management/api/project/event/TaskProcessingController.java
+++ urban-management-api/src/main/java/com/unicom/urban/management/api/project/event/TaskProcessingController.java
@@ -1,14 +1,19 @@
 package com.unicom.urban.management.api.project.event;
 
 import com.unicom.urban.management.common.annotations.ResponseResultBody;
+import com.unicom.urban.management.common.util.FileUploadUtil;
 import com.unicom.urban.management.pojo.dto.StatisticsDTO;
 import com.unicom.urban.management.pojo.vo.DeptVO;
 import com.unicom.urban.management.service.dept.DeptService;
 import com.unicom.urban.management.service.event.TaskProcessingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author 顾志杰
@@ -24,6 +29,9 @@ public class TaskProcessingController {
     @Autowired
     private DeptService deptService;
 
+    @Autowired
+    private FileUploadUtil fileUploadUtil;
+
     /**
      * 授权审批 案件处理 挂账恢复
      *
@@ -38,5 +46,18 @@ public class TaskProcessingController {
     @GetMapping("/dept")
     public List<DeptVO> dept(){
         return deptService.getAll();
+    }
+
+    @PostMapping("/common/uploads")
+    public List<Map<String, Object>> uploadFiles(MultipartFile[] files) throws Exception {
+        List<Map<String, Object>> list = new ArrayList<>();
+        for (MultipartFile file : files) {
+            Map<String, Object> map = new HashMap<>(3);
+            String url = fileUploadUtil.uploadFileToFastDFS(file);
+            map.put("url", url);
+            map.put("fileName", file.getOriginalFilename());
+            list.add(map);
+        }
+        return list;
     }
 }
