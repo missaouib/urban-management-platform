f1cf66ed91a3e389497dbdb7146bfab2709dc4b2
liukai <zhizhufan@foxmail.com>
2021-03-25 19:55:10 +0800

    feat: 修改提示文字

--- urban-management-web/src/main/java/com/unicom/urban/management/web/framework/security/LoginFailureHandler.java
+++ urban-management-web/src/main/java/com/unicom/urban/management/web/framework/security/LoginFailureHandler.java
@@ -50,7 +50,7 @@ public class LoginFailureHandler extends AbstractAuthenticationHandler implement
         }
         if (exception instanceof CaptchaExpiredException) {
             message = 4;
-            result = Result.fail(302, "验证码已过期，情重新输入");
+            result = Result.fail(302, "验证码已过期，请重新输入");
         }
         if (exception instanceof NotDeptException) {
             message = 5;
