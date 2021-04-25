4e4f386ad0dd71c2987df12fe47e7cf08a3397c7
顾志杰 <819162954@qq.com>
2021-04-25 13:37:18 +0800

    feat: 坐标转换接口

--- urban-management-api/src/main/java/com/unicom/urban/management/api/project/gis/GisController.java
+++ urban-management-api/src/main/java/com/unicom/urban/management/api/project/gis/GisController.java
@@ -1,5 +1,6 @@
 package com.unicom.urban.management.api.project.gis;
 
+import cn.hutool.json.JSONObject;
 import com.unicom.urban.management.common.properties.GisServiceProperties;
 import com.unicom.urban.management.common.util.RestTemplateUtil;
 import com.unicom.urban.management.pojo.RestReturn;
@@ -8,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author 顾志杰
@@ -28,5 +32,30 @@ public class GisController {
         RestReturn body = RestTemplateUtil.get(gisServiceProperties.getUrl() + "/distance?geometryBase=" + geometryBase + "&geometry=" + geometry, RestReturn.class).getBody();
         assert body != null;
         return Result.success(body.getData()) ;
+    }
+
+
+
+    @GetMapping("portTransformation")
+    public Result portTransformation(String x, String y){
+        RestReturn body = RestTemplateUtil.post(gisServiceProperties.getUrl() + "/portTransformation",getPointJson(x,y), RestReturn.class).getBody();
+        assert body != null;
+        return Result.success(body.getData()) ;
+    }
+
+
+
+
+
+
+    private JSONObject getPointJson(String x,String y){
+        Map<String, Object> point = new HashMap<>(3);
+        point.put("longitude",x);
+        point.put("latitude",y);
+        point.put("epsg","4326");
+        Map<String, Object> pointApiParam = new HashMap<>(2);
+        pointApiParam.put("point",point);
+        pointApiParam.put("toEPSG","4552");
+        return new JSONObject(pointApiParam);
     }
 }
